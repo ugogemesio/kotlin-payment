72b409efad6973e096828041eb7691d6c960e76c
ugogemesio <ugo.gemesio@gmail.com>
2026-02-17 03:44:27 -0300

    feat: adding containers

--- order-service/src/main/kotlin/gemesio/ugo/order_service/kafka/KafkaConfig.kt
+++ order-service/src/main/kotlin/gemesio/ugo/order_service/kafka/KafkaConfig.kt
@@ -4,6 +4,7 @@ import gemesio.ugo.order_service.order.OrderEvent
 import gemesio.ugo.order_service.order.OrderEventSerializer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
@@ -12,10 +13,13 @@ import org.springframework.kafka.core.KafkaTemplate
 @Configuration
 class KafkaConfig {
 
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private lateinit var bootstrapServers: String
+
     @Bean
     fun producerFactory(): DefaultKafkaProducerFactory<String, OrderEvent> {
         val configProps = mapOf(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to OrderEventSerializer::class.java
         )
